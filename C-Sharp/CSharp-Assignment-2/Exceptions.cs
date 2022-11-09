99fd458de7e6f0a1ec22f8cb7311726e670ce922
Ajinkya066 <Ajinkya.sonwane@emtecinc.com>
2022-11-09 19:07:44 +0530

    C-Sharp Assignment

---
+++ C-Sharp/CSharp-Assignment-2/Exceptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Exceptions
+    {
+        public void Exception()
+        {
+            int a = 10;
+            int b = 0;
+            int c = 0;
+
+            try
+            {
+                c = a / b;
+                string name = null;
+                Console.WriteLine(c);
+                Console.WriteLine(name.Length);
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+   
---
+++ C-Sharp/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using static C_Sharp.Enum;
+
+namespace C_Sharp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inheritance
+            //Bank bank = new Bank();
+            //bank.Run();
+
+            //Email Validation
+            //Email email = new Email();
+            //email.Validation();
+
+            //Enum 
+            //Console.WriteLine("Country {0} {1}", (int)Countries.INDIA, Countries.INDIA);
+            //Console.WriteLine("Country {0} {1}", (int)Countries.AUSTRALIA, Countries.AUSTRALIA);
+            //Console.WriteLine("Country {0} {1}", (int)Countries.USA, Countries.USA);
+            //Console.WriteLine("Country {0} {1}", (int)Countries.JAPAN, Countries.JAPAN);
+            //Console.WriteLine("Country {0} {1}", (int)Countries.FRANCE, Countries.FRANCE);
+            //Console.WriteLine("Country {0} {1}", (int)Countries.ENGLAND, Countries.ENGLAND);
+            //Console.ReadLine();
+
+            //String Operation
+            //StringOperation operation = new StringOperation();
+            //operation.Operation();
+
+            //Exceptions
+            //Exceptions exception = new Exceptions();
+            //exception.Exception();
+
+            //Reading text file
+            //ReadFile readFile= new ReadFile();
+            //readFile.Read();
+
+            //Salary in decending order.
+            //EmployeeSalary employeeSalary = new EmployeeSalary();
+            //employeeSalary.EmployeeData();
+
+            //Student Details
+            //StudentData studentData = new StudentData();
+            //studentData.Display();
+
+            //Generic Classes
+            //GenericClass<int, string> generic = new GenericClass<int, string>();
+            //Console.WriteLine("Addition = {0}",generic.Add(12, 20));
+            //Console.WriteLine("Name = {0}",generic.ConCat("ajin", "kya"));
+
+        }
+    }
+}
