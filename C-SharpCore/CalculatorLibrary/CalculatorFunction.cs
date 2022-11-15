95455d49d4dd0f416beb76d10dc2798e8d2e382e
Ajinkya066 <Ajinkya.sonwane@emtecinc.com>
2022-11-15 16:13:04 +0530

    C-Sharp-Core Assignment

---
+++ C-SharpCore/CalculatorLibrary/CalculatorFunction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculatorLibrary
+{
+    public class CalculatorFunction
+    {
+        public static int Adition(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+        public static int Subtraction(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        public static int Multiplication(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        public static int Division(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
---
+++ C-SharpCore/WebCalculatorCore/Models/CalculatorCore.cs
@@ -0,0 +1,15 @@
+﻿namespace WebCalculatorCore.Models
+{
+    public class CalculatorCore
+    {
+      
+            public int Number1 { get; set; }
+
+            public int Number2 { get; set; }
+
+            public int Result { get; set; }
+
+            public string calculate { get; set; }
+   
+    }
+}
