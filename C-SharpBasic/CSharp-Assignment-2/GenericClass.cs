b534af03ef10a882e3beedf760be47086b85f391
Ajinkya066 <111747636+Ajinkya066@users.noreply.github.com>
2022-11-23 18:39:24 +0530

    Update GenericClass.cs

--- C-SharpBasic/CSharp-Assignment-2/GenericClass.cs
+++ C-SharpBasic/CSharp-Assignment-2/GenericClass.cs
@@ -1,29 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_Sharp
 {
-    internal class GenericClass<INT, STRING>
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    internal class GenericClass<T>
     {
-        public INT MyProperty { get; set; }
-        public STRING MyValue { get; set; }
+        public T MyProperty { get; set; }
 
-        public int Add(INT number1, INT number2)
+        public int Add(T number1, T number2)
         {
             dynamic value1 = number1;
             dynamic value2 = number2;
             return value1 + value2;
         }
-        public string ConCat(STRING string1, STRING string2)
+        public string ConCat(T string1, T string2)
         {
             dynamic stringValue1 = string1;
             dynamic stringValue2 = string2;
             return stringValue1 + stringValue2;
-
         }
-
     }
 }
